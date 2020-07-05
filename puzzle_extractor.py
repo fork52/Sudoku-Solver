8b83f592e9c349f77d941e20f19f6aef33191c81
fork52 <mrunankmistry52@gmail.com>
2020-07-05 23:44:58 +0530

    Removed Pandas dependency.

--- puzzle_extractor.py
+++ puzzle_extractor.py
@@ -1,34 +1,19 @@
-import pandas as pd
-import numpy as np
 from time import time
 from pprint import pprint
 from sudoku_solver import basic_Backtracker , basic_CSP ,basic_CSP_Initial_Domain,CSP_with_MRV
+from csv import reader
 from random import randint
-
 
 def load_random_puzzle(filename):
     '''Loads a random puzzle from the .csv file'''
-    df = pd.read_csv(filename)  
+    csv_file = open(filename,encoding='utf8')
+    csv_reader = reader(csv_file,delimiter= ',')
+    data = [ row for row in csv_reader]   
     puzzle_index = randint(0,2999)
-    puzzle_string = str( df.iat[puzzle_index,1] )
+    puzzle_string = data[puzzle_index+1][2]
     new_puzzle = string_to_puzzle(puzzle_string) 
     return new_puzzle
 
-def read_file(filename ,nrows=10):
-    '''Converts Data/problems.csv string to a puzzle which is a list of lists'''
-    puzzle_list = []
-    puzzle_solns = []
-
-    df = pd.read_csv(filename)
-    for i in range(nrows):
-        # index = int( df.iat[i,0] )
-        puzzle_string = str( df.iat[i,1] )
-        puzzle_soln_string = str( df.iat[i,2] )
-
-        puzzle_list.append (  string_to_puzzle(puzzle_string) )
-        puzzle_solns.append (  string_to_puzzle(puzzle_soln_string) )
-
-    return puzzle_list , puzzle_solns
 
 def string_to_puzzle(puzzle_string):
     '''Convert puzzle string to a 2d list'''
@@ -41,17 +26,37 @@ def string_to_puzzle(puzzle_string):
         count += 1
     return res
 
+def read_file(filename , nrows = 10):
+    '''Converts Data/problems.csv string to a puzzle which is a list of lists'''
+    csv_file = open(filename,encoding='utf8')
+    csv_reader = reader(csv_file,delimiter= ',')
+    data = [ row for row in csv_reader]
+
+    puzzle_list = []
+    puzzle_solns = []
+
+    for i in range(nrows):
+
+        puzzle_string = data[i+1][2]
+        puzzle_soln_string = data[i+1][3]
+
+        puzzle_list.append (  string_to_puzzle(puzzle_string) )
+        puzzle_solns.append (  string_to_puzzle(puzzle_soln_string) )
+
+    return puzzle_list , puzzle_solns
+
+
 if __name__ == "__main__":
     #Load 'nrows' number of sudoku sudoku puzzles
-    puzzles , solns = read_file('data/problems.csv', nrows=10)
+    puzzles , solns = read_file('data/problems.csv', nrows=100)
 
     #Grand total of the time required to solve all puzzles
     total_time = 0
 
     #You can change the type of of solver here.
     # The solvers are essentially classes using different algos to solve Sudoku puzzle
-    # 1) basic_Backtracker 2)basic_CSP 3)basic_CSP_Initial_Domain 4)CSP_with_MRV
-    obj = CSP_with_MRV() 
+    # 1) basic_Backtracker 2)basic_CSP_Initial_Domain 3)basic_CSP 4)CSP_with_MRV
+    obj = CSP_with_MRV()
 
     #Code for testing the time taken by the chosen solver
     i = 0
